187b02665db4180ed805143cf38f3c0d45d83cfa
Chad Parr <cparr2@css.edu>
2016-01-15 13:28:46 -0600

    Initial Commit

---
+++ BB8's Data Bank/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BB8's Data Bank
+//
+//  Created by chad parr on 1/15/16.
+//  Copyright © 2016 chad parr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
